d9aea0bdc786f544e1bff7d0e66df0ee650c34e3
Antonio Alves <aaba03@gmail.com>
2024-10-16 19:55:11 -0300

    feat initial setup of nextjs and nestjs and turbo

---
+++ apps/frontend/additional-env.d.ts
@@ -0,0 +1,18 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production";
+      NEXTAUTH_SECRET: string;
+      APP_JWT_SECRET: string;
+      NEXTAUTH_URL: string;
+
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+
+      NEXT_PUBLIC_SUPABASE_URL: string;
+      SUPABASE_SERVICE_ROLE_KEY: string;
+    }
+  }
+}
+
+export {};
